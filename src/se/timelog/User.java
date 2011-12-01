76334939df001e182f1d20dd2ed96d242a829952
nli <anneli.halldn@student.kyh.se>
2011-12-01 15:58:25 +0100

    import problems

--- src/se/timelog/User.java
+++ src/se/timelog/User.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import se.timelog.UserModel;
 import se.timelog.rmi.MockupRMI;
 
