1cbf650ef48b0d6122fa033ed29f5835aae19c66
Ricard <cordazar@gmail.com>
2011-12-01 14:40:03 +0100

    Updated UserModel and MockupRMI to, hopefully, prevent unneccessary errors.

--- src/se/timelog/UserModel.java
+++ src/se/timelog/UserModel.java
@@ -2,10 +2,10 @@ package se.timelog;
 
 public class UserModel {
 
-	private String email;
-	private String firstName;
-	private String lastName;
-	private String password;
+	private String email = "";
+	private String firstName = "";
+	private String lastName = "";
+	private String password = "";
 
 	public String getEmail() {
 		return email;
