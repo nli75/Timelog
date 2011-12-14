2e15526fddb1416521b97d49a13f16c224790c83
nli <anneli.halldn@student.kyh.se>
2011-12-14 11:21:20 +0100

    refactored TimelogPage as the others

--- src/se/timelog/pages/TimelogPage.java
+++ src/se/timelog/pages/TimelogPage.java
@@ -1,18 +1,20 @@
 package se.timelog.pages;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import se.kyh.ad10.timeloggers.server.entities.Timelog;
-import se.timelog.rmi.MockupRMI;
+import se.timelog.rmi.RMIServerComm;
 
 public class TimelogPage extends RestPage {
 
@@ -39,8 +41,7 @@ public class TimelogPage extends RestPage {
 			timelog.setStart(start);
 			timelog.setDuration(duration);
 
-			MockupRMI mockupRMI = new MockupRMI();
-			ArrayList<String> errorList  = mockupRMI.timelogCreate(timelog);
+			ArrayList<String> errorList  = timelogCreate(timelog);
 			if (errorList.isEmpty()) {
 				request.setAttribute("content", "success");
 				request.getRequestDispatcher("/WEB-INF/views/page_tpl.jsp").forward(request, response);	
@@ -52,6 +53,40 @@ public class TimelogPage extends RestPage {
 		}
 	}
 
+	public ArrayList<String> timelogCreate(Timelog timelog) {
+		ArrayList<String> errorList = new ArrayList<String>();
+		// Title
+		if (timelog.getTitle().length() == 0) {
+			errorList.add("Title not set.");
+		} else {
+			if (timelog.getTitle().length() < 6) {
+				errorList.add("Title too short.");
+			}
+			if (!Validation.isAlphaSpace(timelog.getTitle())) {
+				errorList.add("Title contains illegal character(s).");
+			}
+		}
+		if(errorList.isEmpty()){
+			UUID sessionId = null;
+			try {
+				sessionId = RMIServerComm.get().getSessionId();
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			boolean answer = false;
+			try {
+				answer = RMIServerComm.get().getPublicInterface(sessionId).getTimelogDAO().saveTimeLog(timelog);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(answer == false){
+				errorList.add("Couldn't connect to server");
+			}
+		}
+		return errorList;
+	}
 	@Override
 	public void doUpdate(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
